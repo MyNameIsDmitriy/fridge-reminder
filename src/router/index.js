871e02a225f928a9ff64ef74de0fdd7bbfc57599
MyNameIsDmitriy <gopanchuk.dm@mail.ru>
2024-07-20 17:38:02 +0300

    feat: route protection

--- src/router/index.js
+++ src/router/index.js
@@ -15,11 +15,35 @@ const router = createRouter({
       component: () => import("../views/Register.vue"),
     },
     {
-      path: "/Home",
+      path: "/home",
       name: "Home",
       component: () => import("../views/Home.vue"),
+      meta: { requiresAuth: true },
     },
   ],
+});
+
+const isAuthenticated = () => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    const decodedToken = JSON.parse(atob(token.split(".")[1]));
+    return decodedToken.exp * 1000 > Date.now();
+  } else {
+    return false;
+  }
+};
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (!isAuthenticated()) {
+      next({ path: "/sign-in" });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
